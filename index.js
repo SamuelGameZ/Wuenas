522b1d11188f40c8f8fc48d0ac52c2d72e92fc3d
SamuelGámeZ <124718589+SamuelGameZ@users.noreply.github.com>
2023-06-06 12:58:35 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click', function () {
+    alert('Ya sabia c;')
+
+})
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover', function() {
+    const randomX = parseInt(Math.random() * 90);
+    const randomY = parseInt(Math.random() * 90);
+    noBtn.style.setProperty('top', randomY + '%');
+    noBtn.style.setProperty('left', randomX + '%');
+
+    noBtn.style.setProperty('transform', `translate(-${randomX}%), -${randomY}%`);
+
+})
+
+noBtn.addEventListener('click', function () {
+    const randomX = parseInt(Math.random() * 90);
+    const randomY = parseInt(Math.random() * 90);
+    noBtn.style.setProperty('top', randomY + '%');
+    noBtn.style.setProperty('left', randomX + '%');
+
+    noBtn.style.setProperty('transform', `translate(-${randomX}%), -${randomY}%`);
+
+})
+
+noBtn.setAttribute('tabindex', '-1');
